6b5ee42bb5eb0074e4a1b1dd31b95584389bb25b
Franck Nijhof <frenck@addons.community>
2019-08-21 22:08:57 +0200

    :fire: Removes DNSMasq in favor of Hass.io DNS

---
+++ proxy-manager/rootfs/etc/cont-init.d/nginx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: AdGuard Home
+# Configures NGINX for use with the AdGuard Home server
+# ==============================================================================
+declare hassio_dns
+
+hassio_dns=$(bashio::dns.host)
+sed -i "s/%%hassio_dns%%/${hassio_dns}/g" \
+    /etc/nginx/conf.d/includes/resolvers.conf
