3de0497f9203049cb93a6667b83bbc18111f7296
Franck Nijhof <git@frenck.dev>
2022-06-10 23:34:23 +0200

    ⬆️ Upgrades add-on base image to 12.0.0 (#332)

--- proxy-manager/rootfs/etc/cont-init.d/mysql.sh
+++ proxy-manager/rootfs/etc/cont-init.d/mysql.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Nginx Proxy Manager
 # This file init the MySQL database
